e6eaa5f47c1d6ea3b007e12006ffc4642e5ecc98
Justin.Tsai <h94knight@gmail.com>
2018-01-05 20:52:23 +0800

    安裝 Newtonsoft.JSON 並介接 OpenData

--- Day18_OpenData/Program.cs
+++ Day18_OpenData/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,20 @@ namespace Day18_OpenData
     {
         static void Main(string[] args)
         {
+            var url = "http://data.ntpc.gov.tw/api/v1/rest/datastore/382000000A-000077-002";
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse() as HttpWebResponse;
+            var responseStream = response.GetResponseStream();
+            var reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+            var srcString = reader.ReadToEnd();
+            var jsonData = Newtonsoft.Json.JsonConvert
+                .DeserializeObject<HolidayOpenData>(srcString);
+            foreach (var holiday in jsonData.result.records)
+            {
+                Console.WriteLine($"Date: {holiday.date}, IsHoliday: {holiday.isHoliday}, Category: {holiday.holidayCategory}");
+            }
+            Console.ReadKey();
         }
     }
 }
