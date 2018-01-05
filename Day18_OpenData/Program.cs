eabbd90c1db4146ad674e90ea37bc96fd8e6ef09
Justin.Tsai <h94knight@gmail.com>
2018-01-05 20:07:01 +0800

    加入專案檔案。

---
+++ Day18_OpenData/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day18_OpenData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
